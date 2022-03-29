0a6fbe1a4ac7a543a1b6a5e5852c9c85e0df45d5
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-03-29 13:16:09 +0900

    Execute again to swap left and right

--- display-layout-2.sh
+++ display-layout-2.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-xrandr --output eDP1 --off \
-       --output DP4 --auto --pos 0x0 --primary \
-       --output DP1 --auto --pos 3840x0
+
+CURRENT_NUM_DISPLAYS=$(xrandr | grep -P '^[^ ]+ connected (primary |)\d+x\d+\+\d+\+\d+' | wc -l)
+CURRENT_LEFT=$(xrandr | grep -P '^[^ ]+ connected (primary |)\d+x\d+\+\d+\+\d+' | grep +0+0 | cut -f 1 -d ' ')
+CURRENT_RIGHT=$(xrandr | grep -P '^[^ ]+ connected (primary |)\d+x\d+\+\d+\+\d+' | grep --invert-match +0+0 | cut -f 1 -d ' ')
+
+if [ "$CURRENT_NUM_DISPLAYS" == 2 ]; then
+  xrandr --output eDP1 --off \
+         --output "${CURRENT_RIGHT}" --auto --pos 0x0 --primary \
+         --output "${CURRENT_LEFT}"  --auto --pos 3840x0
+else
+  xrandr --output eDP1 --off \
+         --output DP3 --auto --pos 0x0 --primary \
+         --output DP4 --auto --pos 3840x0
+fi
--- display-layout-3.sh
+++ display-layout-3.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-xrandr --output eDP1 --auto --pos 2023x2160 \
-       --output DP4 --auto --pos 0x0 --primary \
-       --output DP1 --auto --pos 3840x0
+
+CURRENT_NUM_DISPLAYS=$(xrandr | grep -P '^[^ ]+ connected (primary |)\d+x\d+\+\d+\+\d+' | wc -l)
+CURRENT_LEFT=$(xrandr | grep -P '^[^ ]+ connected (primary |)\d+x\d+\+\d+\+\d+' | grep +0+0 | cut -f 1 -d ' ')
+CURRENT_RIGHT=$(xrandr | grep -P '^[^ ]+ connected (primary |)\d+x\d+\+\d+\+\d+' | grep --invert-match +0+0 | grep +0 | cut -f 1 -d ' ')
+
+if [ "$CURRENT_NUM_DISPLAYS" == 3 ]; then
+  xrandr --output eDP1 --auto --pos 2023x2160 \
+         --output "${CURRENT_RIGHT}" --auto --pos 0x0 --primary \
+         --output "${CURRENT_LEFT}"  --auto --pos 3840x0
+else
+  xrandr --output eDP1 --auto --pos 2023x2160 \
+         --output DP4 --auto --pos 0x0 --primary \
+         --output DP3 --auto --pos 3840x0
+fi
